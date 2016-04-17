754ba733fc0a2e85677f4bd14b7c662eda44440c
Alexis Lopez <aalopez@gmail.com>
2016-04-17 17:06:14 -0500

    adds JPA Entity to persist server agents

---
+++ src/main/java/tfactory/server/jpa/entity/ServerAgent.java
@@ -0,0 +1,37 @@
+package tfactory.server.jpa.entity;
+
+import cesarhernandezgt.dto.AgentDto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * ServerAgent Entity
+ * Created by aalopez on 4/16/16.
+ */
+@Entity(name = "SERVER_AGENT")
+public class ServerAgent {
+    @Id
+    private String path;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    /**
+     * Creates a new instance of the entity based on the information of the AgentDto param.
+     *
+     * @param dto Where the information comes from.
+     * @return New instance of the entity class based on the param's data.
+     */
+    public static ServerAgent of(AgentDto dto) {
+        ServerAgent server = new ServerAgent();
+        server.setPath(dto.getPath());
+
+        return server;
+    }
+}
