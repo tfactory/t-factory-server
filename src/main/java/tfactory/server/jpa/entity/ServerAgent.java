ff4fdca3ecc3d8ef96d374ee480ff959ef23cea0
Alexis Lopez <aalopez@gmail.com>
2016-04-24 11:56:06 -0500

    adds attributes to the ServerAgent entity to match the required information on the server.xhtml page.

--- src/main/java/tfactory/server/jpa/entity/ServerAgent.java
+++ src/main/java/tfactory/server/jpa/entity/ServerAgent.java
@@ -14,18 +14,57 @@ import javax.persistence.Table;
 @Table(name = "SERVER_AGENT") //this is required to map database tables
 public class ServerAgent {
 
-    /**
-     * Path to the agent on a remote server
-     */
     @Id
     private String path;
 
+    private String hostname;
+
+    private String status;
+
+    private String versionAgent;
+
+    /**
+     * Path to the agent on a remote server
+     */
     public String getPath() {
         return path;
     }
 
     public void setPath(String path) {
         this.path = path;
+    }
+
+    /**
+     * Hostname.
+     */
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    /**
+     * Server status.
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    /**
+     * Server agent version.
+     */
+    public String getVersionAgent() {
+        return versionAgent;
+    }
+
+    public void setVersionAgent(String version) {
+        this.versionAgent = version;
     }
 
     /**
@@ -37,6 +76,9 @@ public class ServerAgent {
     public static ServerAgent from(AgentDto dto) {
         ServerAgent server = new ServerAgent();
         server.setPath(dto.getPath());
+        server.setHostname(dto.getHostname());
+        server.setStatus(dto.getStatus());
+        server.setVersionAgent(dto.getVersionAgent());
 
         return server;
     }
