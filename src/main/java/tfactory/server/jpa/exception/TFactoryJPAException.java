55aeea00faeef16c6fca6b422478e17b3e5cf790
Alexis Lopez <aalopez@gmail.com>
2016-04-24 13:07:22 -0500

    fixes custom TFactoryJPAException since exception code was not assigned.

--- src/main/java/tfactory/server/jpa/exception/TFactoryJPAException.java
+++ src/main/java/tfactory/server/jpa/exception/TFactoryJPAException.java
@@ -29,6 +29,7 @@ public class TFactoryJPAException extends Exception {
      */
     public TFactoryJPAException(TFactoryExceptionCodeEnum code, String message) {
         super(message);
+        this.exceptionCode = code;
     }
 
     /**
@@ -41,6 +42,7 @@ public class TFactoryJPAException extends Exception {
      */
     public TFactoryJPAException(TFactoryExceptionCodeEnum code, String message, Throwable cause) {
         super(message, cause);
+        this.exceptionCode = code;
     }
 
     /**
@@ -52,6 +54,7 @@ public class TFactoryJPAException extends Exception {
      */
     public TFactoryJPAException(TFactoryExceptionCodeEnum code, Throwable cause) {
         super(cause);
+        this.exceptionCode = code;
     }
 
     /**
@@ -69,6 +72,7 @@ public class TFactoryJPAException extends Exception {
      */
     protected TFactoryJPAException(TFactoryExceptionCodeEnum code, String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
+        this.exceptionCode = code;
     }
 
     /**
